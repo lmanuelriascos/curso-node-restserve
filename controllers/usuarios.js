bf7ee34ab91fdb962811428d741f69c0e557adf6
Manuel Riascos <lmanuelriascos@estudiante.uniajc.edu.co>
2022-09-09 19:22:09 -0500

    Primer comit

---
+++ controllers/usuarios.js
@@ -0,0 +1,63 @@
+const { response, request } = require('express');
+
+
+const usuariosGet =(req, res) => {
+
+    const {q, nombre = 'No name', apikey, page = 1, limit } = req.query;
+    
+    res.json({
+       
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    });
+
+  }
+
+  const usuariosPost =(req, res = response) => {
+
+    const {nombre, edad} = req.body;
+
+    res.json({
+       
+        msg: 'post API - usuariosPost',
+        nombre,
+        edad
+    });
+  }
+
+  const usuariosPut =(req, res = response) => {
+
+    const  id   = req.params.id;
+    res.json({
+       msg: 'put API - usuariosPut',
+       id
+    });
+  }
+
+  const usuariosPatch =(req, res) => {
+    res.json({
+       
+        msg: 'patch API - usuariosPatch'
+    });
+  }
+
+  const usuariosDelete =(req, res) => {
+    res.json({
+       
+        msg: 'delete API - usuariosDelete'
+    });
+  }
+
+
+
+  module.exports = {
+      usuariosGet,
+      usuariosPost,
+      usuariosPut,
+      usuariosPatch,
+      usuariosDelete,
+  }
